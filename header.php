007bed49b02d3cb044af3a12cdf5ecef7cc22be1
firas <fkmkh11@gmail.com>
2023-12-08 07:33:37 +0200

    configure the global header of the  site

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">
+        <img src="f-logo-and-symbols-template-vector-removebg-preview.png" alt="Firas company logo" height="70" class="mr-2">Firas company system
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item active">
+        <a class="nav-link" href="showEmployees.php">show Employees</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="removeEmployee.php">remove Employee</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="updateEmployee.php">update employee data</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="login.php">login</a>
+    </li>
+    
+
+</ul>
+    </div>
+</nav>';
+?>
